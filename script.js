6055dc9510bb3202272fa90f048bccff2badd8ac
Juniiie <jrbuking@gmail.com>
2022-05-14 00:34:12 +1000

    Used API
    Added API link to generate the quotes instead of using object lists

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
 "use strict";
 
-//Motivational Quotes Objects
-const adviceObjs = {
-  1: "1% better everyday",
-  2: "Fail early, Fail Often, but always fail forward",
-  3: "Comparing your shortcomings against the strengths of others is a silly thing to do",
-  4: "Take charge of your thoughts",
-  5: "You have to think about your past to remind you about what you want for your future",
-  6: "Dreams, without goals are just dreams",
-  7: "Only when you identify and accept your weaknesses will you finally stop running from your past",
-  8: "Distance yourself, for yourself",
-  9: "Who checked up on you? Exactly. No one.",
-  10: "Your focus is in your control. You lose focus only if you allow it",
-  11: "Never put yourself in a situation where you could lose yourself",
-  12: `It's on you. Are you going to make today count?`,
-  13: `Don't be afraid to grind alone, the sun is always alone and still shines`,
-  14: `Sometimes it's good to be alone, gives you time to reflect where you've been`,
-  15: `Remember, you didn't come this far just to give up`,
-  16: `Sometimes it takes certain things falling apart, for better things to fall into place`,
-  17: `Sometimes it takes losing what you're settling for, to remind you of what you truly deserve`,
-  18: `Sometimes it takes the most uncomfortable paths to lead your life to the most beautiful place`,
-  19: `Your current situation is not your final destination`,
-  20: `Choose growth over company`,
-  21: `You have to understand that there's a process to everthing and you've got to trust it`,
+// //Motivational Quotes Objects
+// const adviceObjs = {
+//   1: "1% better everyday",
+//   2: "Fail early, Fail Often, but always fail forward",
+//   3: "Comparing your shortcomings against the strengths of others is a silly thing to do",
+//   4: "Take charge of your thoughts",
+//   5: "You have to think about your past to remind you about what you want for your future",
+//   6: "Dreams, without goals are just dreams",
+//   7: "Only when you identify and accept your weaknesses will you finally stop running from your past",
+//   8: "Distance yourself, for yourself",
+//   9: "Who checked up on you? Exactly. No one.",
+//   10: "Your focus is in your control. You lose focus only if you allow it",
+//   11: "Never put yourself in a situation where you could lose yourself",
+//   12: `It's on you. Are you going to make today count?`,
+//   13: `Don't be afraid to grind alone, the sun is always alone and still shines`,
+//   14: `Sometimes it's good to be alone, gives you time to reflect where you've been`,
+//   15: `Remember, you didn't come this far just to give up`,
+//   16: `Sometimes it takes certain things falling apart, for better things to fall into place`,
+//   17: `Sometimes it takes losing what you're settling for, to remind you of what you truly deserve`,
+//   18: `Sometimes it takes the most uncomfortable paths to lead your life to the most beautiful place`,
+//   19: `Your current situation is not your final destination`,
+//   20: `Choose growth over company`,
+//   21: `You have to understand that there's a process to everthing and you've got to trust it`,
+// };
+
+// const btnClick = function () {
+//   let RandQuoteNumber = Math.floor(
+//     Math.random() * Object.keys(adviceObjs).length + 1
+//   );
+//   //   let quoteNumer = "Advice #" + RandQuoteNumber;
+//   let quoteNumer = RandQuoteNumber;
+//   document.querySelector("#advNum").textContent = quoteNumer;
+//   // console.log(adviceObjs[`${RandQuoteNumber}`]);
+//   let quote = adviceObjs[`${RandQuoteNumber}`];
+//   document.querySelector("#advText").textContent = quote;
+//   //console.log(x);
+// };
+// //this.onload = btnClick;
+// //console.log(adviceObjs);
+// //console.log(Object.keys(adviceObjs).length); //gets length of objects
+
+// document.querySelector(".main__cardCircle").addEventListener("click", btnClick);
+
+const fetchAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  document.querySelector("#advNum").textContent = data.slip.id;
+  document.querySelector("#advText").textContent = data.slip.advice;
+
+  console.log(data);
 };
 
 const btnClick = function () {
-  let RandQuoteNumber = Math.floor(
-    Math.random() * Object.keys(adviceObjs).length + 1
-  );
-  //   let quoteNumer = "Advice #" + RandQuoteNumber;
-  let quoteNumer = RandQuoteNumber;
-  document.querySelector("#advNum").textContent = quoteNumer;
-  // console.log(adviceObjs[`${RandQuoteNumber}`]);
-  let quote = adviceObjs[`${RandQuoteNumber}`];
-  document.querySelector("#advText").textContent = quote;
-  //console.log(x);
+  console.log("im being cliocked");
+  fetchAdvice();
 };
-//this.onload = btnClick;
-//console.log(adviceObjs);
-//console.log(Object.keys(adviceObjs).length); //gets length of objects
 
+fetchAdvice();
 document.querySelector(".main__cardCircle").addEventListener("click", btnClick);
